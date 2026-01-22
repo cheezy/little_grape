2614c9d191552958c010ebcaa1f7c4df2dbb7dfd
Jeffrey S. Morgan <cheezy@industriallogic.com>
2026-01-22 12:51:42 -0500

    added apple touch icon

--- lib/little_grape_web.ex
+++ lib/little_grape_web.ex
@@ -17,7 +17,7 @@ defmodule LittleGrapeWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images uploads favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images uploads favicon.ico robots.txt apple-touch-icon.png apple-touch-icon-precomposed.png)
 
   def router do
     quote do
